6a83754a71d4d95af818b636709b4684b7389e6b
Jessica <1530466@upv.edu.mx>
2018-06-11 05:37:39 -0500

    practica12_Fase3

---
+++ practica12JessicaFase3/views/modules/tiendas.php
@@ -0,0 +1,46 @@
+<?php
+if(!$_SESSION["validar"]){
+    echo "<script>window.location.href='index.php?action=ingresar'</script>";
+    exit();
+}
+
+?>
+<div class="content-wrapper">
+    <section class="content">
+        <div class="container-fluid" style="padding: 40px;">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <h3 class="card-title" style="display: inline-block;">Tiendas</h3>
+                        </div>
+                        <a href="index.php?action=registrarTienda"><button type="button" class="btn btn-success" style="color: white;">
+                                Agregar tienda &nbsp&nbsp<i class="right fa fa-plus"></i></button></a>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Agregado el</th>
+                            <th>Acciones</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $vistaTiendas = new ProductosController();
+                        $vistaTiendas-> vistaTiendasController();
+                        ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+</div>
+</section>
+</div>
